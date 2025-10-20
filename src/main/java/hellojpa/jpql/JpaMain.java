532772699f0e98045cb0078ed54c5c69dfa4f374
LeeTaeYoung <xodud5080@naver.com>
2025-10-20 12:55:15 +0900

    fetch join - basic

--- src/main/java/hellojpa/jpql/JpaMain.java
+++ src/main/java/hellojpa/jpql/JpaMain.java
@@ -18,27 +18,39 @@ public class JpaMain {
 		tx.begin();
 
 		try{
-			Team team = new Team();
-			em.persist(team);
+			Team teamA = new Team();
+			teamA.setName("팀A");
+			em.persist(teamA);
+
+			Team teamB = new Team();
+			teamB.setName("팀B");
+			em.persist(teamB);
 
 			Member member1 = new Member();
-			member1.setUsername("관리자1");
-			member1.changeTeam(team);
+			member1.setUsername("회원1");
+			member1.changeTeam(teamA);
 			em.persist(member1);
 
 			Member member2 = new Member();
-			member2.setUsername("관리자2");
-			member2.changeTeam(team);
+			member2.setUsername("회원2");
+			member2.changeTeam(teamA);
 			em.persist(member2);
+
+			Member member3 = new Member();
+			member3.setUsername("회원3");
+			member3.changeTeam(teamB);
+			em.persist(member3);
 
 			em.flush();
 			em.clear();
 
-			String query = "select m.username from Team t join t.members m";
-			List<Collection> result = em.createQuery(query, Collection.class)
+			String query = "select distinct t from Team t join fetch t.members";
+			List<Team> result = em.createQuery(query, Team.class)
 					.getResultList();
 
-			System.out.println("result = " + result);
+			for (Team team : result){
+				System.out.println("team = " + team.getName() + ", member = " + team.getMembers());
+			}
 
 			tx.commit();
 		} catch (Exception e) {
