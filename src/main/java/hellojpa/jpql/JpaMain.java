919674fa01bbcba284e02c20ace0603501ca053e
LeeTaeYoung <xodud5080@naver.com>
2025-10-20 14:11:26 +0900

    entity direct use

--- src/main/java/hellojpa/jpql/JpaMain.java
+++ src/main/java/hellojpa/jpql/JpaMain.java
@@ -44,17 +44,12 @@ public class JpaMain {
 			em.flush();
 			em.clear();
 
-			String query = "select t from Team t ";
-			List<Team> result = em.createQuery(query, Team.class)
-					.setFirstResult(0)
-					.setMaxResults(2)
-					.getResultList();
+			String query = "select m from Member m where m = :member";
+			Member findMember = em.createQuery(query, Member.class)
+					.setParameter("member", member1)
+					.getSingleResult();
 
-			System.out.println("result = " + result.size());
-
-//			for (Team team : result){
-//				System.out.println("team = " + team.getName() + ", member = " + team.getMembers());
-//			}
+			System.out.println("findMember = " + findMember);
 
 			tx.commit();
 		} catch (Exception e) {
