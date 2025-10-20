19767459995007a007e3939698a263b9e49a9178
LeeTaeYoung <xodud5080@naver.com>
2025-10-20 12:29:33 +0900

    path expression

--- src/main/java/hellojpa/jpql/JpaMain.java
+++ src/main/java/hellojpa/jpql/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa.jpql;
 
 import jakarta.persistence.*;
 
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -18,17 +19,26 @@ public class JpaMain {
 
 		try{
 			Team team = new Team();
-			team.setName("teamA");
 			em.persist(team);
 
-			Member member = new Member();
-			member.setUsername("관리자");
-			member.setAge(60);
-			member.changeTeam(team);
-			em.persist(member);
+			Member member1 = new Member();
+			member1.setUsername("관리자1");
+			member1.changeTeam(team);
+			em.persist(member1);
+
+			Member member2 = new Member();
+			member2.setUsername("관리자2");
+			member2.changeTeam(team);
+			em.persist(member2);
 
 			em.flush();
 			em.clear();
+
+			String query = "select m.username from Team t join t.members m";
+			List<Collection> result = em.createQuery(query, Collection.class)
+					.getResultList();
+
+			System.out.println("result = " + result);
 
 			tx.commit();
 		} catch (Exception e) {
